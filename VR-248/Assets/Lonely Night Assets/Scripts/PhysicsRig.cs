1a7ac127ab14f762a2a0d29027276e60e2892396
b00kworm4lyf3 <136265146+b00kworm4lyf3@users.noreply.github.com>
2024-06-13 22:32:14 -0700

    final touches (for now)
    Teleport anchors, new skybox, floating stars

--- VR-248/Assets/Lonely Night Assets/Scripts/PhysicsRig.cs
+++ VR-248/Assets/Lonely Night Assets/Scripts/PhysicsRig.cs
@@ -4,27 +4,28 @@ using UnityEngine;
 
 public class PhysicsRig : MonoBehaviour{
     public Transform playerHead;
-    public Transform leftController;
-    public Transform rightController;
+    // public Transform leftController;
+    // public Transform rightController;
 
     public ConfigurableJoint headJoint;
-    public ConfigurableJoint leftHandJoint;
-    public ConfigurableJoint rightHandJoint;
+    // public ConfigurableJoint leftHandJoint;
+    // public ConfigurableJoint rightHandJoint;
 
     public CapsuleCollider bodyCollider;
 
     public float bodyHeaightMin = 0.5f;
     public float bodyHeightMax = 2;
 
+    //UNCOMMENT EVERYTHING FOR PHYS HANDS AND HEAD - REMEMBER TO REF ON PHISICSRIG COMPONANT
     void FixedUpdate(){
         bodyCollider.height = Mathf.Clamp(playerHead.localPosition.y, bodyHeaightMin, bodyHeightMax);
         bodyCollider.center = new Vector3(playerHead.localPosition.x, bodyCollider.height/2, playerHead.localPosition.z);
 
-        leftHandJoint.targetPosition = leftController.localPosition;
-        leftHandJoint.targetRotation = leftController.localRotation;
+        // leftHandJoint.targetPosition = leftController.localPosition;
+        // leftHandJoint.targetRotation = leftController.localRotation;
 
-        rightHandJoint.targetPosition = rightController.localPosition;
-        rightHandJoint.targetRotation = rightController.localRotation;
+        // rightHandJoint.targetPosition = rightController.localPosition;
+        // rightHandJoint.targetRotation = rightController.localRotation;
 
         headJoint.targetPosition = playerHead.localPosition;
     }
